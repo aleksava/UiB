4720f41b836fd473ccbf99af02b24e89d533c181
aleksava <Alek@vage.com>
2016-09-09 14:28:55 +0200

    Finished vistid.sh

--- DAT103/vistid.sh
+++ DAT103/vistid.sh
@@ -5,20 +5,17 @@ read -p "Event name please: " EVENT
 
 num=$(grep $EVENT $1 | cut -d " " -f2)
 
-#noEntries="0 "
+totalFor=0
 
-#if [ "$num" == "$noEntries" ]; then
-#  echo "Sorry, we couldn't find that command"
-#fi
+if [ ${#num} -eq 1 ]; then
+  numFor=$(echo $num | cut -d " " -f1)
+  totalFor=$((numFor+totalFor))
+else
+  for((i=1; i<=${#num}/2; i++))
+  do
+    numFor=$(echo $num | cut -d " " -f$i)
+    totalFor=$((numFor+totalFor))
+  done
+fi
 
-num1=$(echo $num | cut -d " " -f1)
-num2=$(echo $num | cut -d " " -f2)
-num3=$(echo $num | cut -d " " -f3)
-
-#echo ${#num}
-
-total=0
-total=$((num1+num2+num3))
-
-echo $num
-echo "This command has been run $total times."
+echo "This command has been run a total of $totalFor minutes."
