ba656d83b4fcb627c7cf034a44a2e7747041ad77
aleksavaYoga14 <Alek@vage.com>
2017-02-27 15:23:46 +0100

    Zeroth works

--- INF144/Oblig1/MarkovZeroth.java
+++ INF144/Oblig1/MarkovZeroth.java
@@ -1,32 +1,45 @@
 import java.io.*;
-import java.util.Vector;
 import java.util.HashMap;
 import java.util.Random;
 
 class MarkovZeroth {
 
-    public static HashMap<Character, Integer> mChain = new HashMap<Character, Integer>();
+    private static HashMap<Character, Integer> mChain = new HashMap<Character, Integer>();
     
+    // char array of the alphabet
+    private static char[] alphabet = new char[] {'a','b','c','d','e','f',
+                                                'g','h','i','j','k','l',
+                                                'm','n','o','p','q','r',
+                                                's','t','u','v','w','x',
+                                                'y','z','æ','ø','å',' '};
+
     public static void main (String[] args) {
 
-        char[] alphabet = new char[] {'a','b','c','d','e','f',
-                                      'g','h','i','j','k','l',
-                                      'm','n','o','p','q','r',
-                                      's','t','u','v','w','x',
-                                      'y','z','æ','ø','å',' '};
+
         char start = 'a';
 
+        // fills the hashmap with all the values it should have to avoid
+        // nullpointerexceptions (in theory)
         for(int i = 0; i < alphabet.length; i++) {
             mChain.put(alphabet[i], new Integer(0));
+
+            // debugging
+            //System.out.println("Put: " + alphabet[i] + " val: " + new Integer(0));
         }
 
+        // initiates the analysis of the text
         readFile("Folktale.txt");
-        
+
+        // prints out the generated text
+        System.out.println(generateText(1000));
+
+        // prints out stats for each of the letters in the alphabet
         for(int i = 0; i < alphabet.length; i++) {
             System.out.println(alphabet[i] + ": " +  mChain.get(alphabet[i]));
         }
     }
 
+    // readFile() analyses the given file, gives stats of each character
     public static void readFile(String fileName) {
         BufferedReader br = null;
         FileReader fr = null;
@@ -39,11 +52,13 @@ class MarkovZeroth {
             String[] arr;
 
             while((str = br.readLine()) != null) {
+                str = str.toLowerCase();
                 arr = str.split(" ");
-                int val = mChain.get(' ').intValue();
-                mChain.put(' ', new Integer(val + 1));
+
                 
                 for(int i = 0; i < arr.length; i++) {
+                    int val = mChain.get(' ').intValue();
+                    mChain.put(' ', new Integer(val + 1));
                     addChar(arr[i]);
                 }
                 arr = null;
@@ -56,17 +71,70 @@ class MarkovZeroth {
 
     public static void addChar(String text) {
         char[] letters = text.toCharArray();
+        int value = 0;
 
+        // debugging
+        /* System.out.println("Word: " + text);
+        for(char c : letters) {
+            System.out.print("Char: " + c + " ");
+        } 
+
+        System.out.println();
+        */
+        
         for(int i = 0; i < letters.length; i++) {
+            value = mChain.get(letters[i]).intValue();
+            mChain.put(letters[i], new Integer(value + 1));
+        }
+    }
+
+    public static String generateText(int length) {
+
+        int[] prob = new int[30];
+        String out = "";
+
+        // Premature optimization is the root of all evil
+        // Can be more efficient by using StringBuilder.
+        // StringBuilder out = new StringBuilder();
+
+        // Gathering the probabilities in one place for each char
+        for(int i = 0; i < prob.length; i++) {
+            if(i == 0) 
+                prob[i] = mChain.get(alphabet[i]).intValue();
+            else
+                prob[i] = (prob[i-1] + mChain.get(alphabet[i]).intValue());
+        }
+
+        /* Debugging
+        
+        for(int i = 0; i < prob.length; i++) {
+            System.out.println(alphabet[i] + ": " + prob[i]);
+        } */
+
+        // Generating the output String
+        for(int i = 0; i < length; i++) {
+            int num = 0;
+            Random rnd = new Random();
+            num = rnd.nextInt(prob[29] + 1);
             
-            if(i == 0) {
-                mChain.put(letters[i], new Integer(1));
-            }
-            else {
-                
-                int value = mChain.get(letters[i]).intValue();
-                mChain.put(letters[i], new Integer(value + 1));
+            for(int j = 0; j < prob.length; j++) {
+                if(j == 0) {
+                    if(num < prob[j]) {
+                        out += alphabet[j];
+                        break;
+                    }
+                }
+                else {
+                    if(num < prob[j] && num > prob[j-1]) {
+                        out += alphabet[j];
+                        break;
+                    }
+                }
             }
         }
+
+        // Marking the end of the built String with a period
+        out += ".";
+        return out;
     }
 }
